8d841aea82e17dc41396c63b3fda01dc4d6b8548
Annie David <E20CSE068@bennett.edu.in>
2023-02-15 22:48:37 +0530

    updated data.js files

---
+++ src/data.js
@@ -0,0 +1,43 @@
+export const Locations = [
+	{ id: 1, name: 'Cafes' },
+	{ id: 2, name: 'Library'},
+	{ id: 3, name: 'Cabin'},
+]
+
+export const videos = [
+    {
+        id: 1, locationId: 1, name: 'Cottage Core Cafe',
+        video: 'https://youtube.com/watch?v=dx3GxpitvbY&feature=shares'
+    },
+    {
+        id: 2, locationId: 1, name: 'Modern Cafe',
+        video: 'https://youtube.com/watch?v=qU8o3_T5y5M&feature=shares'
+    },
+    {
+        id: 3, locationId: 1, name: 'Minimilastic Cafe',
+        video: 'https://youtube.com/watch?v=dx3GxpitvbY&feature=shares'
+    },
+    {
+        id: 4, locationId: 2, name: 'Dark Academia Library',
+        video: 'https://youtube.com/watch?v=4vIQON2fDWM&feature=shares'
+    },
+    {
+        id: 5, locationId: 2, name: 'Cozy Library',
+        video: 'https://youtube.com/watch?v=YQc4WT0yDH4&feature=shares'
+    },
+    {
+        id: 6, locationId: 2, name: 'Old Library',
+        video: 'https://youtube.com/watch?v=irdN1XAby6I&feature=shares'
+    },
+    {
+        id: 7, locationId: 3, name: 'Cozy Cabin',
+        video: 'https://youtube.com/watch?v=-mirqViyITY&feature=shares'
+    },
+    {
+        id: 8, locationId: 3, name: 'Cozy Hut Ambience Cabin',
+        video: 'https://youtube.com/watch?v=zvW1T4tpl9A&feature=shares'
+    },
+
+
+
+]
